9f6693587bbfd6a560fec450c033ba3f59a3c56d
rumpeltin <n@rumpeltin.io>
2026-07-06 14:08:09 +0100

    build(lint): configure browser and node environments

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,16 +1,42 @@
-const js = require('@eslint/js');
-const globals = require('globals');
+const js = require("@eslint/js");
+const globals = require("globals");
 
 module.exports = [
-  { ignores: ['eslint.config.cjs', 'node_modules/**', 'img/**', 'files/**'] },
-  js.configs.recommended,
   {
-    files: ['**/*.js'],
+    ignores: [
+      "eslint.config.cjs",
+      "node_modules/**",
+      "img/**",
+      "files/**",
+      "dist/**",
+    ],
+  },
+
+  js.configs.recommended,
+
+  {
+    files: ["src/**/*.js"],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: 'script',
-      globals: { ...globals.browser },
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+      },
     },
-    rules: { "no-unused-vars": 'warn', "eqeqeq": 'error' },
-  }
+    rules: {
+      "no-unused-vars": "warn",
+      eqeqeq: "error",
+    },
+  },
+
+  {
+    files: ["scripts/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 ];
